4c2d53124f761139599a70d9f2553cd25a84ca55
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-09-05 18:42:09 +0300

    Create tunnel class.

---
+++ DimensionMesh/Assets/Script/Data/Tunnel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Script;
+
+public class Tunnel
+{
+
+    public ArrayList tunnelHexagonsList;
+    public ArrayList tunnelMeshList;
+    private Plane tunelPlane;
+
+    public Tunnel()
+    {
+
+    }
+
+
+
+
+}
